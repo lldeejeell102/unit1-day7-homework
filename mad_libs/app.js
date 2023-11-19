c00e3dfe2e27189ca3dc18018aa33c23b4b29d94
Daniel Ji <daniel.ji0923@gmail.com>
2023-11-19 13:53:29 -0800

    went to office hours and Joy helped me work my original method work

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -2,76 +2,65 @@
 console.log("hello world")
 
 // empty object
-let words = {
-    // number: " ",
-    // adjective: " ",
-    // pluralNoun: " ",
-    // adverb: " ",
-    // anotherAdjective: " "
-}
+// let words = {
+//     number: " ",
+//     adjective: " ",
+//     pluralNoun: " ",
+//     adverb: " ",
+//     anotherAdjective: " "
+// }
 
 // loop that prompts user for each entry needed
 // then assigns them to the correct object & element
-const startMadLib = () => {
-    for (let word in words) {
-        const answer = prompt(`Please input ${word}`);
-        words[word] = answer
-    };
-    console.log(words)
-    completedMadLibs();
-}
-
-// prints out completed Mad Libs
-const completedMadLibs = () => {
-    alert(`Once upon a time a group of ${words.number} General Assembly graduates got together and made a startup called ${words.adjective} Technologies. Their goal was to create smart ${words.pluralNoun}. They approached the challenge ${words.adverb} which ultimately lead them to ${words.anotherAdjective} fame.`)
-};
-startMadLib()
-
-
-
-// function startMadLib () {
-//         words.number = prompt(`Give me a number`)
-//         madLibAdjective()
+// const startMadLib = () => {
+//     for (let word in words) {
+//         const answer = prompt(`Please input ${word}`);
+//         words[word] = answer
+//     };
+//     console.log(words)
+//     completedMadLibs();
 // }
+
+// // prints out completed Mad Libs
+// const completedMadLibs = () => {
+//     alert(`Once upon a time a group of ${words.number} General Assembly graduates got together and made a startup called ${words.adjective} Technologies. Their goal was to create smart ${words.pluralNoun}. They approached the challenge ${words.adverb} which ultimately lead them to ${words.anotherAdjective} fame.`)
+// };
+// startMadLib()
+
+
+let words = {}
+function startMadLib () {
+        words.number = prompt(`Give me a number`)
+        words.adjective = prompt(`Give me an adjective`)
+        words.pluralNoun = prompt(`Give me a plural noun`)
+        words.adverb = prompt(`Give me an adverb`)
+        words.anotherAdjective = prompt(`Give me an another ajective`)
+        alert(`Once upon a time a group of ${words.number} General Assembly graduates got together and made a startup called ${words.adjective} Technologies. Their goal was to create smart ${words.pluralNoun}. They approached the challenge ${words.adverb} which ultimately lead them to ${words.anotherAdjective} fame.`)
+}
+startMadLib()
 
 // function madLibAdjective () {
 //     let adjective = prompt(`Give me an adjective`)
-//     if (_.isString(response) === true){
 //         words.adjective = adjective
 //         pluralNoun()
 //     }
-//         alert(`that's not an adjective, try again`)
-//         madLibAdjective()
-// }
 
 // function pluralNoun () {
 //     let pluralNoun = prompt(`Give me a plural noun`)
-//     if (_.isString(response) === true){
 //         words.pluralNoun = pluralNoun
 //         adverb()
 //     }
-//         alert(`that's not a plural noun, try again`)
-//         pluralNoun()
-// }
 
 // function adverb () {
 //     let adverb = prompt(`Give me an adverb`)
-//     if (_.isString(response) === true){
 //         words.adverb = adverb
 //         madLibAdjective2()
 //     }
-//         alert(`that's not an adverb, try again`)
-//         adverb()
-// }
 
 // function madLibAdjective2 () {
 //     let anotherAdjective = prompt(`Give me another adjective`)
-//     if (_.isString(response) === true){
 //         words.anotherAdjective = anotherAdjective
 //     }
-//         alert(`that's not a adjective, try again`)
-//         madLibAdjective2()
-// }
 
 
 
