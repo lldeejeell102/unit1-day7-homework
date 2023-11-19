a4a781a64bf5d422dc9b905b529f129d72939f9a
Daniel Ji <daniel.ji0923@gmail.com>
2023-11-18 19:14:44 -0800

    started madlibs assignment

---
+++ mad_libs/app.js
@@ -0,0 +1,73 @@
+// testing connection
+console.log("hello world")
+
+
+let words = {
+    // number:
+    // adjective:
+    // pluralNoun:
+    // adverb:
+    // anotherAdjective:
+}
+
+
+function startMadLib () {
+    let number = prompt(`Give me a number`)
+    if (Number.isInteger(response) === true){
+        words.number = number
+        madLibAdjective()
+    }
+        alert(`that's not a number, try again`)
+        startMadLib()
+}
+
+function madLibAdjective () {
+    let adjective = prompt(`Give me an adjective`)
+    if (_.isString(response) === true){
+        words.adjective = adjective
+        pluralNoun()
+    }
+        alert(`that's not an adjective, try again`)
+        madLibAdjective()
+}
+
+function pluralNoun () {
+    let pluralNoun = prompt(`Give me a plural noun`)
+    if (_.isString(response) === true){
+        words.pluralNoun = pluralNoun
+        adverb()
+    }
+        alert(`that's not a plural noun, try again`)
+        pluralNoun()
+}
+
+function adverb () {
+    let adverb = prompt(`Give me an adverb`)
+    if (_.isString(response) === true){
+        words.adverb = adverb
+        madLibAdjective2()
+    }
+        alert(`that's not an adverb, try again`)
+        adverb()
+}
+
+function madLibAdjective2 () {
+    let anotherAdjective = prompt(`Give me another adjective`)
+    if (_.isString(response) === true){
+        words.anotherAdjective = anotherAdjective
+    }
+        alert(`that's not a adjective, try again`)
+        madLibAdjective2()
+}
+
+
+
+function runMadLibs () {
+    startMadLib()
+    prompt(`Once upon a time a group of ${words.number} General Assembly graduates
+    got together and made a startup called ${words.adjective} Technologies.
+    Their goal was to create smart ${words.pluralNoun}.
+    They approached the challenge ${words.adverb}
+    which ultimately lead them to ${words.anotherAdjective} fame.`)    
+}
+runMadLibs()
